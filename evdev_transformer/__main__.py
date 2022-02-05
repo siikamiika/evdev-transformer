e79f4240ab65b9e598a8b029db662d9a55979143
siikamiika <siikamiika@users.noreply.github.com>
2022-02-05 20:50:10 +0200

    config cli argument

--- evdev_transformer/__main__.py
+++ evdev_transformer/__main__.py
@@ -1,6 +1,8 @@
+import sys
+
 from .config import ConfigManager
 from .hub import Hub
 
-config_manager = ConfigManager('example_config.json')
+config_manager = ConfigManager(sys.argv[1])
 hub = Hub(config_manager)
 hub.start()
