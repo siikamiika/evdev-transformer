3d2687fa8c70734498071b6b63e5f3416f358a12
siikamiika <siikamiika@users.noreply.github.com>
2022-02-09 18:56:23 +0200

    wait for key release before starting

--- evdev_transformer/__main__.py
+++ evdev_transformer/__main__.py
@@ -1,7 +1,11 @@
 import sys
+import time
 
 from .config import ConfigManager
 from .hub import Hub
+
+# enough time to release the enter key when starting manually
+time.sleep(1)
 
 config_manager = ConfigManager(sys.argv[1])
 hub = Hub(config_manager)
